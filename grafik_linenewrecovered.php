b2786f4dcf347d46f73f8d9cc01a53cb964f93d0
AyuFakhiraArdini <100138682+AyuFakhiraArdini@users.noreply.github.com>
2022-06-14 02:13:20 +0700

    Add files via upload

---
+++ grafik_linenewrecovered.php
@@ -0,0 +1,48 @@
+<?php
+include('koneksi.php');
+$label = ["India","S.Korea","Turkey","Vietnam","Japan","Iran","Indonesia","Malaysia","Thailand","Israel"];
+ 
+for($id = 1;$id < 13;$id++)
+{
+	$query = mysqli_query($koneksi,"select sum(new_recovered) as new_recovered from tb_newrecovered where id='$id'");
+	$row = $query->fetch_array();
+	$jumlah_produk[] = $row['new_recovered'];
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat Grafik Menggunakan Chart JS</title>
+	<script type="text/javascript" src="chart.js"></script>
+</head>
+<body>
+	<div style="width: 800px;height: 800px">
+		<canvas id="myChart"></canvas>
+	</div>
+ 
+ 
+	<script>
+		var ctx = document.getElementById("myChart").getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: <?php echo json_encode($label); ?>,
+				datasets: [{
+					label: 'Grafik Covid',
+					data: <?php echo json_encode($jumlah_produk); ?>,
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+	</script>
+</body>
+</html>
